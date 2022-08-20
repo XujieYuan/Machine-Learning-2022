f24a35374a9e7537243f09cf0827c3c9b3b3fa7e
Xujie Yuan <xyua0019@student.monash.edu>
2022-08-20 11:34:29 +0800

    TODO 1

--- Project1/get_input_args.py
+++ Project1/get_input_args.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                   
-# REVISED DATE: 
+# PROGRAMMER: Xujie Yuan
+# DATE CREATED: 2022.08.20
+# REVISED DATE: 2022.08.20
 # PURPOSE: Create a function that retrieves the following 3 command line inputs 
 #          from the user using the Argparse Python module. If the user fails to 
 #          provide some or all of the 3 inputs, then the default values are
@@ -39,10 +39,11 @@ def get_input_args():
      parse_args() -data structure that stores the command line arguments object  
     """
     # Create Parse using ArgumentParser
-    
+    parser = argparse.ArgumentParser()
     # Create 3 command line arguments as mentioned above using add_argument() from ArguementParser method
-    
-    
-    # Replace None with parser.parse_args() parsed argument collection that 
+    parser.add_argument('--dir', type=str, default='pet_images/', help='path to the folder of pet images')
+    parser.add_argument('--arch', type=str, default='vgg', help='CNN model architecture')
+    parser.add_argument('--dogfile', type=str, default='dognames.txt', help='text file of dog names')
+    # Replace None with parser.parse_args() parsed argument collection that
     # you created with this function 
-    return None
+    return parser.parse_args()
