6321db9e23d55bceb769aa6fced1549f790c61aa
Xujie Yuan <xyua0019@student.monash.edu>
2022-08-29 11:07:50 +0800

    TODO 2

--- Project1/get_pet_labels.py
+++ Project1/get_pet_labels.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
-# REVISED DATE: 
+# PROGRAMMER: Xujie Yuan
+# DATE CREATED: 2022.08.21
+# REVISED DATE: 2022.08.21
 # PURPOSE: Create the function get_pet_labels that creates the pet labels from 
 #          the image's filename. This function inputs: 
 #           - The Image Folder as image_dir within get_pet_labels function and 
@@ -42,4 +42,13 @@ def get_pet_labels(image_dir):
     """
     # Replace None with the results_dic dictionary that you created with this
     # function
-    return None
+    results_dic = dict()
+    in_files = listdir(image_dir)
+    for idx in range(0, len(in_files), 1):
+        if in_files[idx][0] != ".":
+            if in_files[idx] not in results_dic:
+                results_dic[in_files[idx]] = [in_files[idx].split("_")[0].lower().strip()]
+            else:
+                print("** Warning: Duplicate files exist in directory:", in_files[idx])
+
+    return results_dic
