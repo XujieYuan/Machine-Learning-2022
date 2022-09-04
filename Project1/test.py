d65e9b2de5a481ad0700472b899c6f6db9140b12
Xujie Yuan <xyua0019@student.monash.edu>
2022-09-04 17:18:12 +0800

    test for loop quiz 2

--- Project1/test.py
+++ Project1/test.py
@@ -1,10 +1,8 @@
-# Write a for loop that iterates over the names list to create a usernames list. To create a username for each name, make everything lowercase and replace spaces with underscores. Running your for loop over the list:
-names = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
-usernames = []
+# Write a for loop that uses range() to iterate over the positions in usernames to modify the list. Like you did in the previous quiz, change each name to be lowercase and replace spaces with underscores. After running your loop, this lis
+usernames = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
 
 # write your for loop here
-for name in names:
-    name = name.replace(' ', '_')
-    name = name.lower()
-    usernames.append(name)
+for idx in range(len(usernames)):
+    usernames[idx] = usernames[idx].lower().replace(" ", "_")
+
 print(usernames)
