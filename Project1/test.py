3f16142b2882f69bed8b2dc7317bbeeb2686fdf0
Xujie Yuan <xyua0019@student.monash.edu>
2022-09-04 14:30:15 +0800

    test for loop quiz

--- Project1/test.py
+++ Project1/test.py
@@ -1,37 +1,10 @@
-# Imports only listdir function from OS module
-from os import listdir
+# Write a for loop that iterates over the names list to create a usernames list. To create a username for each name, make everything lowercase and replace spaces with underscores. Running your for loop over the list:
+names = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
+usernames = []
 
-# Retrieve the filenames from folder pet_images/
-filename_list = listdir("pet_images/")
-
-# Print 10 of the filenames from folder pet_images/
-print("\nPrints 10 filenames from folder pet_images/")
-for idx in range(0, 10, 1):
-    print("{:2d} file: {:>25}".format(idx + 1, filename_list[idx]))
-
-
-
-# # Sets pet_image variable to a filename
-# pet_image = "Boston_terrier_02259.jpg"
-#
-# # Sets string to lower case letters
-# low_pet_image = pet_image.lower()
-#
-# # Splits lower case string by _ to break into words
-# word_list_pet_image = low_pet_image.split("_")
-#
-# # Create pet_name starting as empty string
-# pet_name = ""
-#
-# # Loops to check if word in pet name is only
-# # alphabetic characters - if true append word
-# # to pet_name separated by trailing space
-# for word in word_list_pet_image:
-#     if word.isalpha():
-#         pet_name += word + " "
-#
-# # Strip off starting/trailing whitespace characters
-# pet_name = pet_name.strip()
-#
-# # Prints resulting pet_name
-# print("\nFilename=", pet_image, "   Label=", pet_name)
+# write your for loop here
+for name in names:
+    name = name.replace(' ', '_')
+    name = name.lower()
+    usernames.append(name)
+print(usernames)
